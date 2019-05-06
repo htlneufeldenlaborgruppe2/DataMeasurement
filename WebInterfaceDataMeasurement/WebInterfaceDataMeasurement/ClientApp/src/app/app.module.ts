c7ddb9e8392ccecc25ee887c0315858015c53a71
Lorenz Winkler <winlor01@gmail.com>
2019-05-06 22:50:34 +0200

    i lOvE npM.... Finished to include module ChartsModule after just working for about 4 hours. Upgraded nearly all packages to newest version (Find and Replaced 5.2.0 with 7.2.0, afterwards npm audit fix --force).

--- WebInterfaceDataMeasurement/WebInterfaceDataMeasurement/ClientApp/src/app/app.module.ts
+++ WebInterfaceDataMeasurement/WebInterfaceDataMeasurement/ClientApp/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { OverViewDataComponentComponent } from './over-view-data-component/over-view-data-component.component';
+import { ChartsModule } from 'ng2-charts';
 @NgModule({
   declarations: [
     AppComponent,
@@ -24,6 +25,7 @@ import { OverViewDataComponentComponent } from './over-view-data-component/over-
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    ChartsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
